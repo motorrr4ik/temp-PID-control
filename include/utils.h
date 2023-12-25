f2619d2b2c619b1d14176651a35c471f3785e773
Nikita Popov <motorrr4ik@iMac.local>
2023-12-25 16:46:44 +0300

    Constants for peripheral added

--- include/utils.h
+++ include/utils.h
@@ -1,13 +1,14 @@
 //Peripheral macros
 #define TIM3_PSC 450
 #define TIM3_ARR 1000
-#define TIM4_PSC 3
-#define TIM4_ARR 3
+#define TIM4_PSC 4500
+#define TIM4_ARR 1000
 
 //Logical macros
 #define REF_VOLTAGE 3.3
 #define MAX_ADC_VALUE 4096
 #define AIM_TEMP 90
-#define K_P 2.5
-#define K_I 2.5
-#define K_E 2.5
+#define K_P 1.2
+#define K_I 0.1
+#define K_E 0.1
+#define dT  0.1
