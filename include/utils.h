1f114d74fe535c4451b95f50a54ea94cc50d81d2
Nikita Popov <motorrr4ik@iMac.local>
2024-01-18 15:28:24 +0300

    temperature division and coeffs adjustment

--- include/utils.h
+++ include/utils.h
@@ -7,8 +7,9 @@
 //Logical macros
 #define REF_VOLTAGE 3.3
 #define MAX_ADC_VALUE 4096
-#define AIM_TEMP 90
-#define K_P 1.2
-#define K_I 0.1
-#define K_E 0.1
+#define AIM_TEMP_UPPER 55
+#define AIM_TEMP_LOWER 30
+#define K_P 45.0
+#define K_I 20.0
+#define K_E 0.01
 #define dT  0.1
