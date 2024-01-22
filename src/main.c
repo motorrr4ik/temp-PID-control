772f8a29ba746ef19bf77240073f9cdef5c02512
Nikita Popov <motorrr4ik@iMac.local>
2024-01-22 11:48:23 +0300

    USART2 initialization

--- src/main.c
+++ src/main.c
@@ -26,19 +26,33 @@ void RCCInit(){
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;    //GPIOA ports activation
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;     //TIM3 activation
     RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;     //TIM4 activation
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;   //USART2 activation
 }
 
 void GPIOAInit(){
-    GPIOA->MODER |= GPIO_MODER_MODE0;   //Enable analog mode for PA0 pin
-    GPIOA->MODER |= GPIO_MODER_MODE6_1; //Enable alternate function mode for PA6 
-    GPIOA->MODER |= GPIO_MODER_MODE1_0; //Enable general purpose output mode for PA1
+    GPIOA->MODER |= GPIO_MODER_MODE0;   //Set PA0 to analog mode
+    GPIOA->MODER |= GPIO_MODER_MODE6_1; //Set PA6 to alternate function mode
+    GPIOA->MODER |= GPIO_MODER_MODE1_0; //Set PA1 to general purpose output mode
     GPIOA->AFR[0]|= GPIO_AFRL_AFRL6_1;  //Enable alternate function register for PA6 pin, TIM3 CH1 PWM mode
     GPIOA->BSRR  |= GPIO_BSRR_BS1;      //Set PA1 output to high
+    GPIOA->MODER |= GPIO_MODER_MODE2_1; //Set PA2 to alternate function mode
+    GPIOA->MODER |= GPIO_MODER_MODE3_1; //Set PA3 to alternate function mode
+    GPIOA->AFR[0]|= GPIO_AFRL_AFRL2_0 
+                   |GPIO_AFRL_AFRL2_1
+                   |GPIO_AFRL_AFRL2_2;  //Enable alternate function register for PA2 pin, USART2 TX
+    GPIOA->AFR[0]|= GPIO_AFRL_AFRL3_0 
+                   |GPIO_AFRL_AFRL3_1
+                   |GPIO_AFRL_AFRL3_2;  //Enable alternate function register for PA3 pin, USART2 RX
+}
+
+void USART2Init(){
+    USART2->CR1 |= USART_CR1_UE;            //USART enable
+    USART2->BRR = SYSTEM_CORE_CLOCK/9600;   //Set USART speed to 9600 B/s  
 }
 
 void TIM3Init(){
     TIM3->CR1 |= TIM_CR1_CEN;           //Enable TIM3
-    TIM3->PSC = TIM3_PSC - 1;           //Set TIM3 prescalser: look into utils.h file
+    TIM3->PSC = TIM3_PSC - 1;           //Set TIM3 prescalser: check out utils.h file for values
     TIM3->ARR = TIM3_ARR - 1;           //Set TIM3 arr: check out utils.h file for values
     TIM3->CNT = 0;                      //Set TIM3 counter 
     TIM3->CCMR1 |= TIM_CCMR1_OC1M_1     //Enable PWM generation on TIM3 Channel1 
@@ -91,6 +105,7 @@ int main(){
     //Peripheral initialization
     RCCInit();
     GPIOAInit();
+    USART2Init();
     TIM3Init();
     TIM4Init();
     ADC1Init();
@@ -105,7 +120,7 @@ int main(){
 
 void TIM4_IRQHandler(){
     TIM4->SR &= ~TIM_SR_UIF;    //Reset interrupt flag
-    //Condition to check wheter temp is achieved
+    //Condition to check wheter aim temp is achieved
     if(currentError <= TEMP_DELTA){
         switchPeltier();
     }
