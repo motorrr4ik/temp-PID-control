36c00b8b2d3adb4209e68a3a64259ebf4b91be72
Nikita Popov <motorrr4ik@iMac.local>
2024-01-30 14:41:22 +0300

    TIM4 interrupt frequency chanched. Added value for PWM freeze

--- include/utils.h
+++ include/utils.h
@@ -4,7 +4,7 @@
 #define SYSTEM_CORE_CLOCK 16000000
 #define TIM3_PSC 160
 #define TIM3_ARR 1000
-#define TIM4_PSC 1600
+#define TIM4_PSC 160
 #define TIM4_ARR 1000
 
 //Logical macros
@@ -12,9 +12,10 @@
 #define MAX_ADC_VALUE 4096
 #define AIM_TEMP_UPPER 55
 #define AIM_TEMP_LOWER 30
-#define TEMP_DELTA 0.2
+#define TEMP_DELTA 0.15
+#define ACHIEVED_TEMP_KEEPER 385
 #define K_P 45.0
 #define K_I 20.0
 #define K_E 0.01
-#define dT  0.1
+#define dT  0.01
 #endif
