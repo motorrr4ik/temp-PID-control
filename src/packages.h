b3227faaf4b2524e286227477cae556976024198
Nikita Popov <motorrr4ik@iMac.local>
2024-04-04 15:04:14 +0300

    new data structures for new stand

--- src/packages.h
+++ src/packages.h
@@ -1,27 +1,63 @@
-#include <stdio.h>
 #ifndef PACKAGES
 #define PACKAGES
+#include <stdio.h>
+#include "utils.h"
 //structs and datatypes
+
+//data for one heating cell
+typedef struct{
+    int8_t coolOrHeatFlag;
+    int8_t aimTemp;
+    float currentTemp;
+    float dutyCycle;
+    int8_t tempValues[CYCLES_NUMBER];
+}heatingCell;
+
+//data from photodiode
+typedef struct{         
+    uint32_t sensorValue1;
+    uint32_t sensorValue2;
+    uint32_t sensorValue3;
+    uint32_t sensorValue4;
+    uint32_t sensorValue5;
+    uint32_t sensorValue6;
+    uint32_t sensorValue7;
+    uint32_t sensorValue8;
+}lightSensorData;
+
+//Temperature values received from heating cells
+typedef struct{
+    uint32_t sensorValue1;
+    uint32_t sensorValue2;
+    uint32_t sensorValue3;
+    uint32_t sensorValue4;
+    uint32_t sensorValue5;
+    uint32_t sensorValue6;
+    uint32_t sensorValue7;
+    uint32_t sensorValue8;
+}heatingCellsTempData;
+
 typedef struct{
     int8_t temp[3];
 }outputPackage;
 
 typedef struct{
-    int8_t enable;
-    int8_t cycles;
+    int8_t standOnOff;
     int8_t pidBorder;
-    int8_t temps[3];
-    int8_t times[3];
-    int8_t coeffs[3];
+    int8_t temps[CYCLES_NUMBER];
+    int8_t timePeriod;
+    int8_t coeffs[CYCLES_NUMBER];
 }inputPackage;
 
+//main process structure
 typedef struct{
-    int8_t coolOrHeatFlag;
+    int8_t standOnOff;
+    heatingCellsTempData cellsTempData;
+    heatingCell cells[HEATING_CELL_NUMBER];
+    lightSensorData sensorsData;
     int8_t currentCycle; 
-    int8_t currentAimTemperature;
     int8_t pidBorder;
-    int8_t currentPeriod;
-    float currentTemperature;
+    int8_t timePeriod;
     float temperatureEquationCoeffs[4];
 }workingPackage;
 #endif
