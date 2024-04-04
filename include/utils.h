38c31cf78622cc0cbf77a35eed9ff8c5efb44a19
Nikita Popov <motorrr4ik@iMac.local>
2024-04-04 15:04:39 +0300

    new defines for updated logic

--- include/utils.h
+++ include/utils.h
@@ -5,11 +5,12 @@
 #define MAX_ADC_VALUE 4096
 #define TEMP_DELTA 0.15
 #define dT  0.01
+#define HEATING_CELL_NUMBER 8
+#define CYCLES_NUMBER 3
 //Periphery macros
 #define SYSTEM_CORE_CLOCK 16000000
 #define TIM3_PSC 160
 #define TIM3_ARR 1000
 #define TIM4_PSC 160
 #define TIM4_ARR 1000
-
 #endif
