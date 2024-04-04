2c0afe9b80133051785347efd79069fb94051340
Nikita Popov <motorrr4ik@iMac.local>
2024-04-04 15:03:48 +0300

    pid update for new stand logic

--- src/PID.c
+++ src/PID.c
@@ -3,36 +3,53 @@
 #include "packages.h"
 //PID calculation
 void calculateDutyCycle(pid* pid, workingPackage *workPack){
-    pid->currentError = workPack->currentAimTemperature - workPack->currentTemperature;
-    if(!workPack->coolOrHeatFlag){
-        pid->currentError = 0 - pid->currentError;
-    }
+    heatingCell currentCell;
+    for(int8_t i = 0; i < HEATING_CELL_NUMBER; ++i){
+        currentCell = workPack->cells[i];
+        pid->currentError = currentCell.aimTemp - currentCell.currentTemp;
+        if(!currentCell.coolOrHeatFlag){
+            pid->currentError = 0 - pid->currentError;
+        }
+    // pid->currentError = workPack->currentAimTemperature - workPack->currentTemperature;
+    // if(!workPack->coolOrHeatFlag){
+    //     pid->currentError = 0 - pid->currentError;
+    // }
     // if(pid->currentError > 0){
     //     workPack->coolOrHeatFlag = 1;
     // }else{
     //     workPack->coolOrHeatFlag = 0;
     //     pid->currentError = fabs(pid->currentError);
     // }
-
-    if(pid->currentError > workPack->pidBorder){
-        pid->dutyCycle = TIM3_ARR;
-    }else{
-        //Check if intergral error between min and max duty cycle value
-        if(((pid->ki * pid->integralError <= TIM3_ARR) && pid->currentError >= 0) || 
-            ((pid->ki * pid->integralError >= 0) && pid->currentError < 0)){
-                pid->integralError += pid->currentError/20;
+        if(pid->currentError > workPack->pidBorder){
+            pid->dutyCycle = TIM3_ARR;
+        }else{
+            //Check if intergral error between min and max duty cycle value
+            if(((pid->ki * pid->integralError <= TIM3_ARR) && pid->currentError >= 0) || 
+                ((pid->ki * pid->integralError >= 0) && pid->currentError < 0)){
+                    pid->integralError += pid->currentError/20;
+            }
+            pid->diffError = (pid->currentError - pid->previousError)/dT;    
+            currentCell.dutyCycle = pid->kp * pid->currentError + pid->ki * pid->integralError + pid->kd * pid->diffError;
+            pid->previousError = pid->currentError;
         }
-        pid->diffError = (pid->currentError - pid->previousError)/dT;    
-        pid->dutyCycle = pid->kp * pid->currentError + pid->ki * pid->integralError + pid->kd * pid->diffError;
-        pid->previousError = pid->currentError;
     }
 }
 
 void setWorkMode(pid* pid, workingPackage* workPack){
-    pid->currentError = workPack->currentAimTemperature - workPack->currentTemperature;
-    if(pid->currentError > 0){
-        workPack->coolOrHeatFlag = 1;
-    }else{
-        workPack->coolOrHeatFlag = 0;
-    }  
+    heatingCell currentCell;
+    for(int8_t i = 0; i < HEATING_CELL_NUMBER; ++i){
+        currentCell = workPack->cells[i];
+        pid->currentError = currentCell.aimTemp - currentCell.currentTemp;
+        if(pid->currentError > 0){
+            currentCell.coolOrHeatFlag = 1;
+        }else{
+            currentCell.coolOrHeatFlag = 0;
+        }  
+    }
+    // pid->currentError = workPack->currentAimTemperature - workPack->currentTemperature;
+    // if(pid->currentError > 0){
+    //     workPack->coolOrHeatFlag = 1;
+    // }else{
+    //     workPack->coolOrHeatFlag = 0;
+    // }  
 }
