e10f0d5ec7c45df805a5667abad660309b8ca110
motorrr4ik <nikita29042001@yandex.ru>
2024-06-18 11:46:11 +0300

    feat: peltier power calculation

--- src/heating_cell/heating_cell.c
+++ src/heating_cell/heating_cell.c
@@ -1,1 +1,15 @@
 #include "heating_cell.h"
+#include "pid_regulator.h"
+
+void switchPeltierMode(heating_cell_t *heating_cell)
+{
+}
+
+void calculatePeltierPower(heating_cell_t *heating_cell)
+{
+    for (int8_t i = 0; i < HEATING_CELL_NUMBER; ++i)
+    {
+        heating_cell->pid_regulator.current_error = heating_cell->temperature.difference;
+        heating_cell->peltier.power               = calculateDutyCycle(&(heating_cell->pid_regulator));
+    }
+}
