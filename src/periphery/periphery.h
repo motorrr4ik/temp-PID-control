1731584fa2292f393db7a485a6b291beeab47679
motorrr4ik <nikita29042001@yandex.ru>
2024-06-20 20:42:07 +0300

    feat: cleanedup

--- src/periphery/periphery.h
+++ src/periphery/periphery.h
@@ -4,12 +4,17 @@
 #include "stm32f4xx.h"
 
 void RCCSetUp(void);
+//-----GPIO setup-----
 void GPIOASetup(void);
 void GPIOBSetup(void);
 void GPIOCSetup(void);
+//-----TIMs setup-----
 void TIM1SetUp(void);
 void TIM3SetUp(void);
+//----USART setup-----
 void USART2SetUp(void);
+void USART2_IRQn(void);
+//----DMA setup-------
 void DMA1SetUp(void);
 void DMA2SetUp(void);
 void ADC1SetUp(void);
