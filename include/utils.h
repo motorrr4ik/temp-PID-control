75355aab26479b21f12750bf24d13324c6cc7e6e
motorrr4ik <nikita29042001@yandex.ru>
2024-06-20 20:30:33 +0300

    fix: max duty cycle value added

--- include/utils.h
+++ include/utils.h
@@ -1,16 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 // Logical macros
-#define REF_VOLTAGE       3.3
-#define MAX_ADC_VALUE     4096
-#define PID_SW_TEMP_DELTA 0.15
-#define dT                0.01
-#define CYCLES_NUMBER     3
+#define REF_VOLTAGE          3.3
+#define MAX_ADC_VALUE        4096
+#define PID_SW_TEMP_DELTA    0.15
+#define dT                   0.01
+#define CYCLES_NUMBER        3
 // Periphery macros
-#define SYSTEM_CORE_CLOCK 16000000
-#define TIM3_PSC          160
-#define TIM3_ARR          1000
-#define TIM1_PSC          160
-#define TIM1_ARR          1000
-#define BAUDRATE          9600
+#define SYSTEM_CORE_CLOCK    16000000
+#define TIM3_PSC             160
+#define TIM3_ARR             1000
+#define TIM1_PSC             160
+#define TIM1_ARR             1000
+#define BAUDRATE             9600
+#define MAX_DUTY_CYCLE_VALUE 1000
 #endif
