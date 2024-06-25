f9fc6e1f0992c272bcd3810acd4c26c9589c39c5
motorrr4ik <nikita29042001@yandex.ru>
2024-06-25 20:08:33 +0300

    fix: filling main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
-#include "heating_cell.h"
+#include "periphery.h"
 #include "pid_regulator.h"
-#include "stm32f446xx.h"
+#include "stand_workflow.h"
+#include "stm32f4xx.h"
+
+static stand_workflow_t stand = {.if_enabled                  = 0,
+                                 .cells_initiated             = 0,
+                                 .temperature_equation_coeffs = {9.418, -40.978, 95.315, -28.127},
+                                 .spi_adc                     = 0,
+                                 .adc                         = 0,
+                                 .input_data                  = 0,
+                                 .output_data                 = 0,
+                                 .cells                       = 0};
 
 int main(void)
 {
+    initPeriphery(&stand);
+    while (1)
+    {
+        mainTask(&stand);
+    }
     return 0;
 }
