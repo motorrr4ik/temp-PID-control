d936974f753c61d4a8cd2bfbf218b36a45146aaf
motorrr4ik <nikita29042001@yandex.ru>
2024-07-09 17:30:09 +0300

    fix: spi setup

--- src/periphery/spi_setup.c
+++ src/periphery/spi_setup.c
@@ -70,4 +70,7 @@
 
 void SPI1SetUp(void)
 {
+    SPI1->CR1 = SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_DFF | SPI_CR1_MSTR |
+                // SPI_CR1_RXONLY |
+                SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_BR_2 | SPI_CR1_BR_0 | SPI_CR1_BR_1;
 }
